886b5ce4764a4e81f54a55ad74749ea3f9be4620
Boston Gordon <bostn.gordon@gmail.com>
2020-05-08 14:24:46 -0400

    Created js.app
    Created js.app per the checkpoint of assignment.

---
+++ app.js
@@ -0,0 +1,48 @@
+function onReady() {
+  let id = 0;
+  let toDos = [];
+  const addToDoForm = document.getElementById('addToDoForm');
+  function createNewToDo() {
+    const newToDoText = document.getElementById('newToDoText');
+    if (!newToDoText.value) { return; }
+    toDos.push({
+     title: newToDoText.value,
+     complete: false
+     id: id.variable
+   });
+   newToDoText.value = '';
+   renderTheUI();
+}
+
+function renderTheUI() {
+  const toDoList = document.getElementById('toDoList');
+  toDoList.textContent = '';
+  toDos.forEach(function(toDo) {
+        const newLi = document.createElement('li');
+        const checkbox = document.createElement('input');
+        checkbox.type = "checkbox";
+
+        newLi.textContent = toDo.title;
+
+        toDoList.appendChild(newLi);
+        newLi.appendChild(checkbox);
+   });
+   let deleteButtion = document.createElement('button');
+      deleteButton.textContent = "Delete";
+      deleteButton.addEventListener('click', function(event){
+        toDoList.removeChild(this.parentElement);
+        renderTheUI()
+      });
+ }
+
+addToDoForm.addEventListener('submit', event => {
+   event.preventDefault();
+   createNewToDo();
+ });
+
+ renderTheUI();
+}
+
+window.onload = function() {
+  onReady();
+};
